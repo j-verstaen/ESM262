05260437c0bad6bdf4b34e547f5e0eb4b2a093cc
Juliette Verstaen <julietteverstaen@ucsb.edu>
2019-05-31 09:22:00 -0700

    editing package description

--- Assignment_4/R/calc_fisheries_data.R
+++ Assignment_4/R/calc_fisheries_data.R
@@ -1,13 +1,10 @@
-#' Abdunance and revenue information of fish caught in Moorea, French Polynesia
+#' Abundance and revenue information of fish caught in Moorea, French Polynesia
 #'
-#' calculate the most frequently caught fish in each location, total revenue for each location, total fisheries revenue sum, and if requested a graph of revenue by location and total revenue (as text)
-#' @param catch_location_data  data frame with columns northside, westside, and eastside (sides of the island), and rows parrotfish, unicronfish, bonito. mahi, yellowfin, and swordfish (fish species caught)
-#' @param price_data data frame with column price (Polynesian Franc/kg) and rows parrotfish, unicronfish, bonito. mahi, yellowfin, and swordfish (fish species caught)
-#'	rain (precip in mm), year, month (integer), day
+#' Calculate the most frequently caught fish in each location, total revenue for each location, total fisheries revenue sum, and if requested a graph of revenue by location and total revenue (as text)
+#' @param catch_location_data  data frame with columns: fish species, northside, westside, and eastside (sides of the island)
+#' @param price_data data frame with fish species and price (Polynesian Franc/kg)
 #' @param graph specify TRUE for output of a graph of revenue by location
-#' @param total_revenue specifiy TRUE for total revenue sum
-#' @return returns a list containing  most frequently caught fish in each location, total revenue for each location, and total fisheries revenue sum
-
+#' @return returns a list containing  most frequently caught fish in each location, revenue by location, revenue by fisheries, total revene, and graph if requested
 
 fish_summary = function(catch_location_data, price_data, graph=FALSE) {
 
